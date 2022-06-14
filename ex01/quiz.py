97db84c55685ea98813d61df2c448d70b3db6877
yamamoto <c0a2113629@edu.teu.ac.jp>
2022-06-14 18:31:42 +0900

    未完成だった英語ゲーム

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,7 +1,12 @@
 import random
-lst = {"サザエの旦那の名前は？":"マスオ",
-      "カツオの妹の名前は？":"ワカメ",
-       "タラオはカツオから見てどんな関係？":"甥"}
+import datetime
+
+st = datetime.datetime.now()
+ed = datetime.datetime.now()
+print((ed-st).seconds)
+lst = {"サザエの旦那の名前は？":["マスオ","ますお"],
+      "カツオの妹の名前は？":["ワカメ","わかめ"],
+       "タラオはカツオから見てどんな関係？":["甥","甥っ子"]}
 
 count = 0
 while count<3:
@@ -9,7 +14,7 @@ while count<3:
     randomlst = random.choice(list(lst.keys()))
     print("問題:")
     print(randomlst)
-    inputlst = input("答えるんだ:")
+    inputlst = input("答えろ:")
     if inputlst== lst[randomlst]:
         print("正解!!!")
         if count==3:
@@ -17,7 +22,7 @@ while count<3:
         else:
             continue
     else:
-        print("出直してこい")
+        print("違います")
         if count ==3:
             break
         else:
