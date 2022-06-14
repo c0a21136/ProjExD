c7688b17e46ff13b58e2813fa171c6453cded76f
yamamoto <c0a2113629@edu.teu.ac.jp>
2022-06-14 18:37:27 +0900

    未完成だった英語ゲーム　#1

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -6,5 +6,38 @@ b = 10
 c = 2
 
 def main():
-    start = datetime.datetime.now()
-    question, anser = shutudai()
+    for xyz in range(answer):
+        m = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        t = []
+        k = []
+        for i in range(c):
+            a=random.randint(0,len(m)-1)
+            t.append(m.pop(a))
+        print("対象文字:")
+        print("".join(t))
+        for i in range(b):
+            b =random.randint(0, len(t)-1)
+            k.append(t.pop(b))
+        print("表示文字")
+        print(".join"(t))
+
+        x = 1
+        for i in range(c):
+            kaitou = input(f"{x}文字目の欠損文字を入れてください")
+            if kaitou in k:
+                print("正解")
+                x+=1
+                k.remove(str(kaitou))
+            else:
+                print("不正解")
+                print("--------------------")
+                if xyz == 4 and len(k)!=0:
+                    print("GAME OVER")
+                break
+            if len(k)==0:
+                print("終了")
+                break
+start = datetime.datetime.now()
+main()
+end = datetime.datetime.now()
+print(end-start)
