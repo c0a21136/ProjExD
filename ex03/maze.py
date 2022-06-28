9487a7241ac0159c19bf894fc54ea83388c73035
yamamoto <c0a2113629@edu.teu.ac.jp>
2022-06-28 15:48:25 +0900

    3限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,24 +11,28 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global cx, cy
+    global cx, cy, mx, my
     delta = {
-            "":[0, 0],
-            "Up":[0,-20],
-            "Down":[0, +20],
-            "Left":[-20, 0],
-            "Right":[+20, 0]}
+           "Up":[0,-1],
+            "Down":[0, +1],
+           "Left":[-1, 0],
+          "Right":[+1, 0]}
     #キー：押されているキーkey/値：移動幅リスト
-    cx, cy = cx+delta[key][0], cy+delta[key][1]
-    canvas.coords("tori", cx, cy)
-    root.after(30, main_proc)
-
-    #if key == "UP": cy -= 20
-    #if key == "Down": cy += 20
-    #if key == "Left": cx -= 20
-    #if key == "Right": cx += 20
+    #cx, cy = cx+delta[key][0], cy+delta[key][1]
     #canvas.coords("tori", cx, cy)
-    #root.after(100, main_proc)
+    #root.after(30, main_proc)
+    try:
+        if maze_bg[my+delta[key][1]][mx+delta[key][0]]==0:
+            my, mx = my+delta[key][1],mx+delta[key][0]
+    except:
+        pass #ここまでがif使わない場合
+    #if key == "UP" and maze_bg[my-1][mx] == 0: my -=1
+    #if key == "Down" and maze_bg[my+1][mx]==0: my += 1
+    #if key == "Left"and maze_bg[my][mx-1]==0: mx -= 1
+    #if key == "Right"and maze_bg[my][mx+1]==0: mx += 1
+    cx, cy = mx*100+50, my*100+50
+    canvas.coords("tori", cx, cy)
+    root.after(100, main_proc)
 
 
 
@@ -44,9 +48,11 @@ if __name__ == "__main__":
     maze_bg = mm.make_maze(15, 9)
     #print(maze_bg) １が壁　0が床
     mm.show_maze(canvas, maze_bg) #canvasにmaze_bgを描く
+    #ディスプレイ設定でプレイ時は100に変えること。
 
     tori = tk.PhotoImage(file="fig/5.png")
-    cx, cy = 300, 400
+    mx, my =1, 1
+    cx, cy = mx*100+50, my*100+50
     canvas.create_image(cx, cy, image=tori, tag="tori")
 
     key = ""
