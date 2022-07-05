cd36bd5c2ef12188b7eee1ced98278691eb7ab34
yamamoto <c0a2113629@edu.teu.ac.jp>
2022-07-05 14:24:44 +0900

    練習３

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -9,12 +9,17 @@ def main():
     screen_sfc = pg.display.set_mode((1600, 900))   #surface
     screen_rect = screen_sfc.get_rect()             #Rect
     bgimg_sfc =pg.image.load("fig/pg_bg.jpg")       #surface
-    bgimg_rect = bgimg_sfc.get_rect()                #Rect
+    bgimg_rect = bgimg_sfc.get_rect()               #Rect
     screen_sfc.blit(bgimg_sfc, bgimg_rect)
     #pg.display.update()　#試し
+    kkimg_sfc = pg.image.load("fig/6.png")          #Surface
+    kkimg_sfc =pg.transform.rotozoom(kkimg_sfc, 0, 2.0) #Surface
+    kkimg_rect = kkimg_sfc.get_rect()               #Rect
+    kkimg_rect.center = 900, 400
 
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rect)
+        screen_sfc.blit(kkimg_sfc, kkimg_rect)
         #練習2
         for event in pg.event.get():
             if event.type == pg.QUIT: 
